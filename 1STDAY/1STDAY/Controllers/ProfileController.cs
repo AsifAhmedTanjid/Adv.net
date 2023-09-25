f6b35db6899a40490e9bb470400ae234825a1230
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2023-09-26 00:39:02 +0600

    first

---
+++ 1STDAY/1STDAY/Controllers/ProfileController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1STDAY.Controllers
+{
+    public class ProfileController : Controller
+    {
+        //public string name = "asif ahmed tanjid";
+        public ActionResult Index()
+        {
+            ViewBag.Name = "Asif Ahmed Tanjid";
+            ViewBag.ID = "20-44249-3";
+            ViewBag.Inst = " AIUB";
+            ViewBag.CGPA = 3.98;
+
+
+            return View();
+        }
+
+        public ActionResult Bio()
+        {
+            ViewBag.Name = "Asif Ahmed Tanjid";
+            ViewBag.ID = "20-44249-3";
+            ViewBag.Inst = " AIUB";
+            ViewBag.CGPA = 3.98;
+            ViewBag.Subject = " BSc in CSE";
+            return View();
+        }
+    }
+
+    
+}
