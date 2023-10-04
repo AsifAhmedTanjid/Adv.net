61cb1dc29e40f45881798c8869b5886a2f29ca14
AsifAhmedTanjid <asifahmednirob44@gmail.com>
2023-10-04 06:00:27 +0600

    validation and anootations

---
+++ VALIDATION/VALIDATION/Controllers/DashBoardController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VALIDATION.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        // GET: DashBoard
+        public ActionResult Profiles()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
---
+++ VALIDATION/VALIDATION/Controllers/RegistrationController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VALIDATION.Models;
+
+namespace VALIDATION.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Home()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel login)
+        {
+            return View(login);
+        }
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(SignUpModel signup)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Profiles","DashBoard");
+            }
+            return View(signup);
+        }
+    }
+}
